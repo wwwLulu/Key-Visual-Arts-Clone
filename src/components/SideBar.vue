2f52b3e956b85a162065538831313145457d3628
wwwLulu <amongthestars9898@gmail.com>
2021-03-05 21:58:36 -0800

    Twitter Embedded

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -20,13 +20,22 @@
             src="@/assets/key_side_banner_sprb.jpg"
             alt="thumbnail"
         />
+        <div class="twitter">
+            <h2>Twitter</h2>
+            <a
+                class="twitter-timeline"
+                data-width="300"
+                data-height="500"
+                href="https://twitter.com/key_official?ref_src=twsrc%5Etfw"
+                >Tweets by Key_Official</a
+            >
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
 .side-bar {
     width: 30em;
-
     margin-top: 2.5rem;
     img {
         width: 100%;
@@ -39,6 +48,17 @@
         display: grid;
         gap: 1rem;
         grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+    }
+    .twitter {
+        h2 {
+            max-width: 30rem;
+            text-align: center;
+            background: var(--color-primary);
+            color: white;
+            padding: 1.2rem 2rem;
+            margin-bottom: 1rem;
+            border-radius: 2rem;
+        }
     }
 }
 </style>
